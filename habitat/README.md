b35ca453bd0e943d969670c4ad29c5405e59f049
Christian Nunciato <chris@nunciato.org>
2018-10-05 09:45:41 -0700

    Add the actual things

---
+++ habitat/README.md
@@ -0,0 +1,9 @@
+# Habitat package: testapp3
+
+## Description
+
+Provide a brief description of the `testapp3` plan / purpose.
+
+## Usage
+
+Describe the general usage for the `testapp3` plan
---
+++ habitat/plan.sh
@@ -0,0 +1,14 @@
+pkg_name=testapp3
+pkg_origin=cnunciato
+pkg_version="0.1.0"
+pkg_maintainer="Christian Nunciato <chris@nunciato.org>"
+pkg_license=("MIT")
+pkg_deps=(cnunciato/testapp2)
+
+do_install() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
